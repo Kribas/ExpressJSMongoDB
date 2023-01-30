b5dab669764842bc01d86bcd5fe091ada988ba1d
Kribas Rimal <carlousrimal@gmail.com>
2023-01-30 12:39:50 +0545

    First Commit

---
+++ routes/routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const router = express.Router();
+const Model = require("../model/model");
+
+// POST Method
+router.post("/post", async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// GET Method
+router.get("/getAll", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// GET by ID method
+router.get("/getOne/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update by ID Method
+router.patch("/update/:id", (req, res) => {
+  res.send("Update by ID API");
+});
+
+// Delete by ID Method
+router.delete("/delete/:id", (req, res) => {
+  res.send("Delete by ID API");
+});
+
+module.exports = router;
